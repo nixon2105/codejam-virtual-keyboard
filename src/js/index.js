7c402186052e0d180cbefe518a8699aeffed58e8
nixon2105 <radivilov1991@mail.ru>
2020-04-04 18:32:23 +0300

    feat: add data of keybord

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,27 @@
 import '../css/style.css';
 import '../css/style.scss';
+import { exportArrays } from './Data';
+
+const importedArrays = exportArrays();
+
+const {
+    keycode,
+    engKeys,
+    rusKeys,
+    engKeysUppercase,
+    rusKeysUppercase,
+    engKeysShift,
+    rusKeysShift
+} = importedArrays;
+
+
+
+        
+
+
+
+
+
+
+
+
